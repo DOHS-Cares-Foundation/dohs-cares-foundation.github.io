f0c6d594decc038388c687ac3c8dba739003c505
Gbemi <oliveajike@gmail.com>
2025-02-23 14:36:42 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# dohs-cares-foundation.github.io
-PreFem is a web app designed to combat femicide in Nigeria
--- src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ComingSoon from '../components/ComingSoon';
-
-const Home = () => {
-  return <ComingSoon />;
-};
-
-export default Home;
